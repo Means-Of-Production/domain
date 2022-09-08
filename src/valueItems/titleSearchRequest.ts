72cb4e7c9aa44bd65f9964a922e1420e5e4086d7
Mathieson Sterling <mathieson@cherre.com>
2022-09-08 09:17:59 -0400

    merge fix

--- src/valueItems/titleSearchRequest.ts
+++ src/valueItems/titleSearchRequest.ts
@@ -1,16 +1,7 @@
-import {ThingTitle} from "./thingTitle";
-
 export class TitleSearchRequest {
     public readonly searchText: string | undefined
 
     constructor(searchText: string | undefined = undefined) {
         this.searchText = searchText
-    }
-
-    public matches(title: ThingTitle): boolean {
-        if(this.name) {
-            return title.name.includes(this.name)
-        }
-        return true
     }
 }
